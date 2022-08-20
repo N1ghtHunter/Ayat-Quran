295bdb8cf523d83cadea4e71af626b85f4897e7f
N1ghtHunter <zamzam2960@gmail.com>
2022-08-20 18:55:08 +0200

    Search bar for all pages + error handling

--- app.js
+++ app.js
@@ -8,23 +8,35 @@ const fs = require("fs");
 const request = require("request");
 var path = require("path");
 const { url } = require("inspector");
-
+const ejsLint = require("ejs-lint");
+var querystring = require("querystring");
+var cookieParser = require("cookie-parser");
 //EJS
 app.use(expressLayouts);
 app.set("view engine", "ejs");
 //Parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cookieParser());
 //app.use(express.static(__dirname)); // Current directory is root
 app.use(express.static(path.join(__dirname, "public"))); //  "public" off of current is root
 
 app.get("/", function (req, res) {
-	const randomAyah = Math.floor(Math.random() * 6232) + 1;
-	const url =
-		"https://api.alquran.cloud/v1/ayah/" +
-		randomAyah +
-		"/editions/quran-uthmani,en.asad,en.pickthall,ar.muyassar,ar.alafasy";
+	var context = req.cookies["context"];
+	var url;
+	res.clearCookie("context", { httpOnly: true });
+	if (context) {
+		url =
+			"https://api.alquran.cloud/v1/ayah/" +
+			context +
+			"/editions/quran-uthmani,en.asad,en.pickthall,ar.muyassar,ar.alafasy";
+	} else {
+		const randomAyah = Math.floor(Math.random() * 6232) + 1;
+		url =
+			"https://api.alquran.cloud/v1/ayah/" +
+			randomAyah +
+			"/editions/quran-uthmani,en.asad,en.pickthall,ar.muyassar,ar.alafasy";
+	}
 	let options = { json: true };
 	request(url, options, (error, response, body) => {
 		if (error) {
@@ -55,7 +67,11 @@ app.get("/", function (req, res) {
 		}
 	});
 });
-
+app.post("/", (req, res) => {
+	let chosenAyah = req.body.ayahNumber;
+	res.cookie("context", chosenAyah, { httpOnly: true });
+	res.redirect("/");
+});
 app.get("/surah/:choosenSurah", (req, res) => {
 	let choosenSurah = req.params.choosenSurah.trim();
 	let url = `https://api.alquran.cloud/v1/surah/${choosenSurah}/editions/quran-uthmani,ar.muyassar,ar.alafasy`;
@@ -65,7 +81,6 @@ app.get("/surah/:choosenSurah", (req, res) => {
 			return console.log(error);
 		}
 		if (!error && res.statusCode == 200) {
-			console.log(body);
 			const surahArray = body.data[0].ayahs;
 			const surahText = surahArray?.map((i) => {
 				return i.text;
@@ -107,13 +122,46 @@ app.post("/search", async (req, res) => {
 			return console.log(error);
 		}
 		if (!error && res.statusCode == 200) {
-			if (body.data) {
+			if (body) {
 				const { count, matches } = body.data;
 				request("http://api.alquran.cloud/v1/surah", options, (err, response, surahLinks) => {
 					res.render("search", { matches, surahLinks: surahLinks.data });
 				});
+			} else {
+				res.redirect("/");
 			}
 		}
+	});
+});
+
+app.use(function (req, res, next) {
+	var err = new Error("Not Found");
+	err.status = 404;
+	next(err);
+});
+// error handlers - these take err object.
+// these are per request error handlers.  They have two so in dev
+// you get a full stack trace.  In prod, first is never setup
+
+// development error handler
+// will print stacktrace
+if (app.get("env") === "development") {
+	app.use(function (err, req, res, next) {
+		res.status(err.status || 500);
+		res.render("error", {
+			message: err.message,
+			error: err,
+		});
+	});
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+	res.status(err.status || 500);
+	res.render("error", {
+		message: err.message,
+		error: {},
 	});
 });
 app.listen(process.env.PORT || 3000, function () {
